8918cd512f0b2d3483c1bddb96cbfc8b6a2e933b
ali <safdar211@hotmail.com>
2015-07-26 08:01:08 +0500

    filter final update

--- db/migrate/20150720125751_add_status_to_products.rb
+++ db/migrate/20150720125751_add_status_to_products.rb
@@ -1,6 +1,6 @@
 class AddStatusToProducts < ActiveRecord::Migration
   def up
-    change_column :products, :status, :string,:default => 'Enabled'
+    add_column :products, :status, :string,:default => 'Enabled'
   end
   def down
     remove_column :products,:status
